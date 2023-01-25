cbeee06b2cc1d1cda5a6b3642ced5ee3762c99f5
vlad <vladislavludinov3006@gmail.com>
2023-01-25 18:45:43 +0300

    input numbers and method counting pozitive numbers

--- Task41/Program.cs
+++ Task41/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите числа через запятую: ");
+string[] numbers = ReadLine()!.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+
+int countNumbers(string[] arr)
+{
+    int count = 0;
+    foreach (var a in arr)
+    {
+        if (int.Parse(a) < 0)
+        {
+            count =+1;
+        }
+    }
+    return count;
+}
