8701b7b06e03a55b5a5b8ce907119bb617e3cb0b
vlad <vladislavludinov3006@gmail.com>
2023-01-25 19:41:52 +0300

    method founding x and y added

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -3,7 +3,17 @@
 WriteLine("Даны уравнения:");
 WriteLine("y = k1 * x + b1");
 WriteLine("y = k2 * x + b2");
-Write("Введите k1, b1, k2, b1 через запятую:");
-string [] variables = ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries);
+Write("Введите k1, b1, k2, b2 через запятую:");
+string [] variables = ReadLine()!.Split(",", StringSplitOptions.RemoveEmptyEntries);
 
 
+
+double [] countXY(string[] varib)
+{
+    double [] xy = new double [2];
+    double k = Convert.ToDouble(varib[0])-Convert.ToDouble(varib[2]);
+    double b = Convert.ToDouble(varib[3])-Convert.ToDouble(varib[1]);
+    xy[0] = b/k;
+    xy[1] = Convert.ToDouble(varib[0])*xy[0]+Convert.ToDouble(varib[1]);
+    return xy;
+}
