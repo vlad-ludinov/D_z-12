ecdb28af9743c31de6de48ea30902fc5e347ae49
vlad <vladislavludinov3006@gmail.com>
2023-01-25 19:21:40 +0300

    input variables added

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Даны уравнения:");
+WriteLine("y = k1 * x + b1");
+WriteLine("y = k2 * x + b2");
+Write("Введите k1, b1, k2, b1 через запятую:");
+string [] variables = ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+
